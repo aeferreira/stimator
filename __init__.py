63e5a8e549e11c8525734ed6c3608d59594fae77
aeferreira <ferreiae@gmail.com>
2010-04-08 18:06:08 +0000

    ditto 3

--- __init__.py
+++
@@ -1,6 +0,0 @@
-"""Let's transform this into a package"""
-
-from model import Model, react, transf, state, variable, forcing
-from analysis import solve, plot
-from timecourse import readTimeCourses
-
