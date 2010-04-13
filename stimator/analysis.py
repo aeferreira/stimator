2df7e05295bdf21d5598fdafb74cfe6eeb9d36be
aeferreira <ferreiae@gmail.com>
2010-04-13 15:42:20 +0000

    Updated analysis.py examples with function read_model()

--- stimator/analysis.py
+++ stimator/analysis.py
@@ -149,21 +149,23 @@ def test():
         print "%-8s= %f" % (x, value)
 
     #print '---------------- EXAMPLE 2 ------------------'
-    m2 = Model("Branched pathway")
-    m2.v1 = react("A -> B", rate = "k1*A")
-    m2.k1 = 10
-    m2.v2 = react("B -> C", rate = "k2*B**0.5")
-    m2.k2 = 5
-    m2.v3 = react("C -> D", rate = "k3*C**0.5")
-    m2.k3 = 2
-    m2.v4 = react("C -> E", rate = "k4*C**0.5")
-    m2.k4 = 8
-    m2.v5 = react("D ->  ", rate = "k5*D**0.5")
-    m2.k5 = 1.25
-    m2.v6 = react("E ->  ", rate = "k6*E**0.5")
-    m2.k6 = 5
-    m2.A  = 0.5
-    m2.init = state(B = 2, C = 0.25, D = 0.64, E = 0.64)
+    m2 = read_model("""
+    title Branched pathway
+    v1 = A -> B, rate = k1*A
+    k1 = 10
+    v2 = B -> C, rate = k2*B**0.5
+    k2 = 5
+    v3 = C -> D, rate = k3*C**0.5
+    k3 = 2
+    v4 = C -> E, rate = k4*C**0.5
+    k4 = 8
+    v5 = D ->  , rate = k5*D**0.5
+    k5 = 1.25
+    v6 = E ->  , rate = k6*E**0.5
+    k6 = 5
+    A  = 0.5
+    init = state(B = 2, C = 0.25, D = 0.64, E = 0.64)
+    """)
 
     #print m2
     times = append(linspace(0.0,5.0,500),linspace(5.0,10.0, 500))
@@ -171,30 +173,34 @@ def test():
     solution2 = solve(m2, tf = 10.0, times=times)
 
     #print '---------------- EXAMPLE 3 ------------------'
-    m3 = Model("Calcium Spikes")
-    m3.v0 = " -> Ca", 1
-    m3.v1 = react(" -> Ca", rate = "B*k1")
-    m3.k1 = 7.3
-    m3.B  = 0.4
-    m3.export = " Ca -> ", 10
-    m3.leak   = "CaComp -> Ca", 1
-    m3.v2     = "Ca -> CaComp", "65 * Ca**2 / (1+Ca**2)"
-    m3.v3     = "CaComp -> Ca", "500*CaComp**2/(CaComp**2+4) * Ca**4/(Ca**4 + 0.6561)"
-    m3.init   = state(Ca = 0.1, CaComp = 0.63655)
+    m3 = read_model("""
+    title Calcium Spikes
+    v0 =  -> Ca, 1
+    v1 =  -> Ca, B*k1
+    k1 = 7.3
+    B  = 0.4
+    export = Ca -> , 10 ..
+    leak   = CaComp -> Ca, 1 ..
+    v2     = Ca -> CaComp, 65 * Ca**2 / (1+Ca**2)
+    v3     = CaComp -> Ca, 500*CaComp**2/(CaComp**2+4) * Ca**4/(Ca**4 + 0.6561)
+    init   = state(Ca = 0.1, CaComp = 0.63655)
+    """)
 
     #print m3
 
     solution3 = solve(m3, tf = 8.0, npoints = 2000)
 
     print '---------------- EXAMPLE 4 ------------------'
-    m4 = Model("Rossler")
-    m4.X1 = variable("X2 - X3")
-    m4.X2 = variable("0.36 * X2 - X1")
-    m4.X3 = variable("X1 *X3 - 22.5 * X3 - 49.6 * X1 + 1117.8")
+    m4 = read_model("""
+    title Rossler
+    X1' = X2 - X3
+    X2' = 0.36 * X2 - X1
+    X3' = X1 *X3 - 22.5 * X3 - 49.6 * X1 + 1117.8
+    init = state(X1 = 19.0, X2 = 47, X3 = 50)
+    """)
     m4.x3 = transf("X3 -50.0")
     m4.x1 = transf("X1 -18.0")
     m4.x2 = transf("X2 -50.0")
-    m4.init = state(X1 = 19.0, X2 = 47, X3 = 50)
 
     print m4
 
