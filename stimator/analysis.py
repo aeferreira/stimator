6a33f30d1a802526fb55ddfa1bdd6a1a0e3bd046
aeferreira <ferreiae@gmail.com>
2014-01-21 16:14:01 +0000

    Added model examples as a submodule of examples. File: models.py.

--- stimator/analysis.py
+++ stimator/analysis.py
@@ -5,7 +5,7 @@
 #         PROJECT S-TIMATOR
 #
 # S-timator Model analysis classes
-# Copyright António Ferreira 2006-2013
+# Copyright António Ferreira 2006-2014
 #----------------------------------------------------------------------------
 import math
 import os
@@ -16,6 +16,7 @@ from modelparser import read_model
 from numpy import *
 from scipy import integrate
 from timecourse import SolutionTimeCourse, Solutions
+from examples import models
 
 import pylab as p
 
@@ -284,17 +285,7 @@ def plot(solutions, show = False, figure = None, style = None, titles=None, ynor
 
 def test():
     print '---------------- EXAMPLE 1 ------------------'
-    m1 = read_model("""
-    title Glyoxalase system
-    glo1 = HTA -> SDLTSH, rate = V1*HTA/(Km1 + HTA)
-    glo2 = SDLTSH -> 2.1 DLac   , rate = V2*SDLTSH/(Km2 + SDLTSH)
-    V1  = 2.57594e-05
-    Km1 = 0.252531
-    V2  = 2.23416e-05
-    Km2 = 0.0980973
-    init = state(SDLTSH = 7.69231E-05, HTA = 0.1357)
-    tf: 4030
-    """)
+    m1 = read_model(models.glyoxalases.text)
 
     print m1
 
@@ -306,23 +297,7 @@ def test():
         print "%-8s= %f" % (x, value)
 
     #print '---------------- EXAMPLE 2 ------------------'
-    m2 = read_model("""
-    title Branched pathway
-    v1 = A -> B, rate = k1*A
-    k1 = 10
-    v2 = B -> C, rate = k2*B**0.5
-    k2 = 5
-    v3 = C -> D, rate = k3*C**0.5
-    k3 = 2
-    v4 = C -> E, rate = k4*C**0.5
-    k4 = 8
-    v5 = D ->  , rate = k5*D**0.5
-    k5 = 1.25
-    v6 = E ->  , rate = k6*E**0.5
-    k6 = 5
-    A  = 0.5
-    init = state(B = 2, C = 0.25, D = 0.64, E = 0.64)
-    """)
+    m2 = read_model(models.branched.text)
 
     #print m2
     times = append(linspace(0.0,5.0,500),linspace(5.0,10.0, 500))
@@ -330,18 +305,7 @@ def test():
     solution2 = solve(m2, tf = 10.0, times=times)
 
     #print '---------------- EXAMPLE 3 ------------------'
-    m3 = read_model("""
-    title Calcium Spikes
-    v0 =  -> Ca, 1
-    v1 =  -> Ca, B*k1
-    k1 = 7.3
-    B  = 0.4
-    export = Ca -> ,       10 ..
-    leak   = CaComp -> Ca, 1 ..
-    v2     = Ca -> CaComp, 65 * Ca**2 / (1+Ca**2)
-    v3     = CaComp -> Ca, 500*CaComp**2/(CaComp**2+4) * Ca**4/(Ca**4 + 0.6561)
-    init   = state(Ca = 0.1, CaComp = 0.63655)
-    """)
+    m3 = read_model(models.ca.text)
 
     #print m3
 
@@ -350,16 +314,7 @@ def test():
 ##     solution3 = solve(m3, tf = 8.0, npoints = 2000)
 
     print '---------------- EXAMPLE 4 ------------------'
-    m4 = read_model("""
-    title Rossler
-    X1' = X2 - X3
-    X2' = 0.36 * X2 - X1
-    X3' = X1 *X3 - 22.5 * X3 - 49.6 * X1 + 1117.8
-    init = state(X1 = 19.0, X2 = 47, X3 = 50)
-    ~ x3 = X3 -50.0
-    ~ x1 = X1 -18.0
-    ~ x2 = X2 -50.0
-    """)
+    m4 = read_model(models.rossler.text)
 
     print m4
 
--- stimator/examples/demo_solve.py
+++ stimator/examples/demo_solve.py
@@ -8,17 +8,7 @@ print __doc__
 print "S-timator version", __version__.fullversion, "(%s)" %__version__.date
 print
 print '---------------- EXAMPLE 1 ------------------'
-m1 = read_model("""
-title Glyoxalase system
-glo1 = HTA -> SDLTSH, V1*HTA/(Km1 + HTA)
-glo2 = SDLTSH ->    , V2*SDLTSH/(Km2 + SDLTSH)
-V1  = 2.57594e-05
-Km1 = 0.252531
-V2  = 2.23416e-05
-Km2 = 0.0980973
-init = state(SDLTSH = 7.69231E-05, HTA = 0.1357)
-""")
-
+m1 = read_model(examples.models.glyoxalases.text)
 print (m1)
 
 solution1 = solve(m1, tf=4030.0)
@@ -29,23 +19,7 @@ for x, value in solution1.last:
     print "%-8s= %f" % (x, value)
 
 #print '---------------- EXAMPLE 2 ------------------'
-m2 = read_model("""
-title Branched pathway
-v1 = A -> B, k1*A
-k1 = 10
-v2 = B -> C, k2*B**0.5
-k2 = 5
-v3 = C -> D, k3*C**0.5
-k3 = 2
-v4 = C -> E, k4*C**0.5
-k4 = 8
-v5 = D ->  , k5*D**0.5
-k5 = 1.25
-v6 = E ->  , k6*E**0.5
-k6 = 5
-A  = 0.5
-init = state(B = 2, C = 0.25, D = 0.64, E = 0.64)
-""")
+m2 = read_model(examples.models.branched.text)
 
 #print m2
 times = append(linspace(0.0, 5.0, 500), linspace(5.0, 10.0, 500))
@@ -53,35 +27,13 @@ times = append(linspace(0.0, 5.0, 500), linspace(5.0, 10.0, 500))
 solution2 = solve(m2, tf=10.0, times=times)
 
 #print '---------------- EXAMPLE 3 ------------------'
-m3 = read_model("""
-title Calcium Spikes
-v0 =  -> Ca, 1
-v1 =  -> Ca, B*k1
-k1 = 7.3
-B  = 0.4
-export = Ca -> ,       10 ..
-leak   = CaComp -> Ca, 1 ..
-v2     = Ca -> CaComp, 65 * Ca**2 / (1+Ca**2)
-v3     = CaComp -> Ca, 500*CaComp**2/(CaComp**2+4) * Ca**4/(Ca**4 + 0.6561)
-init   = state(Ca = 0.1, CaComp = 0.63655)
-""")
-
+m3 = read_model(examples.models.ca.text)
 #print m3
 
 solution3 = solve(m3, tf=8.0, npoints=2000)
 
 print '---------------- EXAMPLE 4 ------------------'
-m4 = read_model("""
-title Rossler
-X1' = X2 - X3
-X2' = 0.36 * X2 - X1
-X3' = X1 *X3 - 22.5 * X3 - 49.6 * X1 + 1117.8
-init = state(X1 = 19.0, X2 = 47, X3 = 50)
-~ x3 = X3 -50.0
-~ x1 = X1 -18.0
-~ x2 = X2 -50.0
-""")
-
+m4 = read_model(examples.models.rossler.text)
 print m4
 
 solution4 = solve(m4, tf=100.0, npoints=2000, outputs="x1 x2 x3")
@@ -98,13 +50,7 @@ solution4.apply_transf(transformation)
 plot([solution1, solution2, solution3, solution4])
 
 print '---------------- EXAMPLE 5 ------------------'
-m5 = read_model("""
-title Lorentz model: sensitivity to initial conditions
-x' = 10*(y-x)
-y' = x*(28-z)-y
-z' = x*y - (8/3)*z
-init = state(x = 1, y = 1, z = 1)
-""")
+m5 = read_model(examples.models.lorentz.text)
 print m5['title']
 s = Solutions(m5['title'])
 ms = ModelSolver(m5, tf=20.0,
