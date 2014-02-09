d971b4230ab3fdc9e1928e6fcc8762fb5f1e16f0
aeferreira <ferreiae@gmail.com>
2014-02-09 16:58:15 +0000

    final preparation for removeMostCrowded() to accept more than one point.

--- stimator/exp_rmc.py
+++ stimator/exp_rmc.py
@@ -1,16 +1,12 @@
 import numpy
 import pylab as pl
 
-def removeMostCrowded(x, knumber, pop_removed=False, distance_fn=None):
+def removeMostCrowded(x, knumber, remove_n = 1, print_removed = False):
 
     """
     Removes a point with the smaller crowiding distance measured as
     the sum of the distances of the points to their k-nearest neighbours.
     x is a dictionary which values are the lists of objective values for each solution.
-    distance_fn
-    is an optional function that takes two points and returns the
-    distance between them.  If distance_fn is None (the default), the
-    Euclidean distance is used.
     """
     
     n_points = len(x)
@@ -20,25 +16,20 @@ def removeMostCrowded(x, knumber, pop_removed=False, distance_fn=None):
         x.popitem()
         return x
     keys = x.keys()
-    keys.sort()
+    keys.sort() # TODO: not necessary??
     n_objs = len(x[keys[0]])
     
-    points = range(n_points)
+    points = range(n_points) #holds current indexes of points still present
     
     #compute matrix of objectives
     obj_matrix = numpy.empty((n_points, n_objs))
     for i,k in enumerate(keys):
         obj_matrix[i, :] = x[k]
 
-    # find keys for extremes
+    # find indexes of extreme points (max and min in each dimension)
     
     maxima = numpy.amax(obj_matrix, axis=0)
     minima = numpy.amin(obj_matrix, axis=0)
-    
-##     print obj_matrix
-##     print maxima
-##     print minima
-##     print
     
     extremes = []
     for i in range(n_objs):
@@ -54,7 +45,7 @@ def removeMostCrowded(x, knumber, pop_removed=False, distance_fn=None):
     print 'extremes', [keys[i] for i in extremes]
 
     #compute distances
-    
+    # TODO: use numpy function
     distanceMatrix = []
     
     for i in range(n_points):
@@ -70,19 +61,27 @@ def removeMostCrowded(x, knumber, pop_removed=False, distance_fn=None):
                 distances.append(d)
         distanceMatrix.append(distances)
     
-    #compute k shortest (note: position 0 after sorting is always 0.0)
-    kdistances =[]
-    only_kdistances = []
+    #sort distances for each point
+    distances_array = []
     for i in points:
-        distances = zip(list(distanceMatrix[i]), range(n_points))
-        distances.sort()
-        kdistances.append(distances[1:knumber+1])
-        only_kdistances.append([ d[0] for d in kdistances[-1]])
+        dd = zip(list(distanceMatrix[i]), range(n_points))
+        dd.sort()
+        distances_array.append(dd)
     
+    #compute k shortest (note: position 0 after sorting is always 0.0)
+    #TODO: repeat from here
+    kdistances =[]
+    ksums = []
+    
+    for i in points:
+        kdistances.append(distances_array[i][1:knumber+1])
+        ksums.append(sum([ d[0] for d in kdistances[-1]]))
+    
+
     print '\nk-distances'
     for k,d in zip(points,kdistances):
         print keys[k],
-        print [ (t1, keys[t2]) for (t1,t2) in d]
+        print [ ("%.3g"%t1, keys[t2]) for (t1,t2) in d]
     print
     
     #find and remove most crowded
@@ -93,14 +92,14 @@ def removeMostCrowded(x, knumber, pop_removed=False, distance_fn=None):
             if k in extremes:
                 distancesAndKeys.append((10**300, k))
             else:
-                distancesAndKeys.append(((sum(only_kdistances[i])), k))
+                distancesAndKeys.append((ksums[i], k))
     else:
         for i,k in enumerate(points):
-            distancesAndKeys.append(((sum(only_kdistances[i])), k))        
+            distancesAndKeys.append((ksums[i], k))      
     mcpoint = min(distancesAndKeys)[1]
 
     mck = keys[mcpoint]
-    if pop_removed:
+    if print_removed:
         mcv = x[mck]
         print 'Most crowded key:', mck, mcv
     del x[mck]
@@ -122,11 +121,12 @@ def pprint(x, showplot=False):
 
 def test():
     x = dict(A=(0,1), B=(0,0), C=(1,1), D=(1.2,0.5), E=(1,0), F=(0,0.5),
-    G=(0.25, 0.75), H=(0.25,0.5), I=(0.5,1), J=(0.5,0.75), K=(0.5,0.5), L=(0.75,0.5))
+    G=(0.25, 0.75), H=(0.25,0.5), I=(0.5,1), J=(0.5,0.75), K=(0.5,0.5), 
+    L=(0.75,0.5))
     print 'initial'
     pprint(x)
     for n in range(15):
-        x = removeMostCrowded(x,3,pop_removed=True)
+        x = removeMostCrowded(x, 3, print_removed=True)
         print '\n----------------------------\nafter removing %d points' % (n+1)
         pprint(x, showplot=False)
 
