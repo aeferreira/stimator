f85666ea4da829761a25558758cd6eee2677dcfb
aeferreira <ferreiae@gmail.com>
2014-02-09 21:09:40 +0000

    Integrated new function removeMostCrowded() in module GDE3solver.py.

--- stimator/GDE3solver.py
+++ stimator/GDE3solver.py
@@ -352,11 +352,15 @@ class GDE3Solver(DESolver):
                 for ndf in nondominated_waves:
                     tempObjDic = {}
                     if len(self.population) < self.populationSize and len(self.population) + len(ndf) >= self.populationSize:
+                        print 'len pop', len(self.population)
+                        print 'pop size', self.populationSize
                         # create a set of solutions to exactly complete pop to populationSize
                         for k in ndf:
                             tempObjDic[k] = self.objectives[k]
-                        while len(self.population) + len(tempObjDic) != self.populationSize:
-                            tempObjDic = removeMostCrowded(tempObjDic, 3)
+                        print 'dict size', len(tempObjDic)
+##                         while len(self.population) + len(tempObjDic) != self.populationSize:
+                        tempObjDic = removeMostCrowded(tempObjDic, 3, len(self.population) + len(ndf) - self.populationSize)
+                        print 'dict size', len(tempObjDic)
                         full = True
                         break
                     elif len(self.population) == self.populationSize:
@@ -602,95 +606,143 @@ class Node:
             self.nodeIndex = -1
 
 
-def removeMostCrowded(x, knumber, pop_removed=False, distance_fn=None):
+def removeMostCrowded(x, knumber = 3, remove_n = 1, verbose = False):
 
     """
     Removes a point with the smaller crowiding distance measured as
     the sum of the distances of the points to their k-nearest neighbours.
-    x is a dictionary which values are the lists of objective values for each solution.
-    distance_fn
-    is an optional function that takes two points and returns the
-    distance between them.  If distance_fn is None (the default), the
-    Euclidean distance is used.
-    This function was written by extensive modification of the Calculate function
-    in the kNN module of the Biopython package.
+    x is a dictionary which values are the point coordinates as a list.
     """
-    if len(x) == 0 :
+    
+    n_points = len(x)
+    if n_points == 0 :
         return x
-    if len(x) < 3:
-        x.popitem()
+    if remove_n < 1:
         return x
-    keys = x.keys()
-    keys.sort()
+##     if n_points < 3:
+##         x.popitem()
+##         return x
+    keys = list(x.keys())
     n_objs = len(x[keys[0]])
     
-    # find keys for extremes
+    points = range(n_points) #holds current indexes of points still present
+    
+    #compute matrix of objectives
+    obj_matrix = numpy.empty((n_points, n_objs))
+    for i,k in enumerate(keys):
+        obj_matrix[i, :] = x[k]
+
+    # find indexes of extreme points (max and min in each dimension)
+    
+    maxima = numpy.amax(obj_matrix, axis=0)
+    minima = numpy.amin(obj_matrix, axis=0)
+    
     extremes = []
     for i in range(n_objs):
-        maximum = x[keys[0]][i]
-        tempMax = [keys[0]]
-        minimum = x[keys[0]][i]
-        tempMin = [keys[0]]
-        for j in keys[1:]:
-            v = x[j][i]
-            if v < minimum:
-                tempMin = [j]
-                minimum = v
-            elif v == minimum:
-                tempMin.append(j)                
-            elif v > maximum:
-                tempMax = [j]
-                maximum = v
-            elif v == maximum:
-                tempMax.append(j)                
-        extremes.extend(tempMin)
-        extremes.extend(tempMax)
+        dimextremes = []
+        for j in points:
+            v = obj_matrix[j][i]
+            if v == minima[i] or v == maxima[i]:
+                dimextremes.append(j)                
+        for j in dimextremes:
+            if j not in extremes:
+                extremes.append(j)
+    extremes.sort()
     
+    if verbose:
+        print 'extreme points', [keys[i] for i in extremes]
+
     #compute distances
-    for k in keys:
-        x[k] = numpy.array(x[k])
-    
-    lista =[]
+    # TODO: use numpy function
     distanceMatrix = []
     
-    for i in range(len(keys)):
+    for i in range(n_points):
         distances = []
-        for j in range(len(keys)):
+        for j in range(n_points):
             if j < i:
                 distances.append(distanceMatrix[j][i])
             elif j ==i:
-                distances.append(0)
+                distances.append(0.0)
             elif j > i:
-                temp = x[keys[i]] - x[keys[j]]
-                dist = numpy.sqrt(numpy.dot(temp,temp))
-                distances.append(dist)
-        distanceMatrix.append(numpy.copy(distances))
-        #compute k shortest (position 0 after sorting iss always 0)
-        distances.sort()
-        if knumber < len(distances):
-            lista.append(distances[1:knumber+1])
-        else:
-            lista.append(distances[1:])
+                temp = numpy.array(obj_matrix[i]) - numpy.array(obj_matrix[j])
+                d = numpy.sqrt(numpy.dot(temp,temp))
+                distances.append(d)
+        distanceMatrix.append(distances)
     
-    #find and remove most crowded
-    distancesAndKeys = []
-    extremes.sort()
-    if keys != extremes:
-        for i,k in enumerate(keys):
-            if k in extremes:
-                distancesAndKeys.append((10**300, k))
-            else:
-                distancesAndKeys.append(((sum(lista[i])), k))
-    else:
-        for i,k in enumerate(keys):
-            distancesAndKeys.append(((sum(lista[i])), k))        
-    if pop_removed:
-        print 'Most crowded key:', x[min(distancesAndKeys)[1]]
-    del x[min(distancesAndKeys)[1]]
-    for i in x: #TODO confirm if this object should be returned as list or Arrays
-        x[i] = list(x[i]) #dto be used by other functions in next generations
-    return x
+    #sort distances for each point
+    distances = []
+    for i in points:
+        dd = zip(list(distanceMatrix[i]), range(n_points))
+        dd.sort()
+        distances.append(dd)
+    
+    #compute k shortest (note: position 0 after sorting is always 0.0)
+    last_removed = None
+    
+    # loop to remove each point
+    for n in range(remove_n):
+        if len(points) == 0:
+            if verbose:
+                print '\nNo more points to remove'
+            return x
 
+        if verbose:
+            print '---------------------------\nRemoving point #%d' % (n+1), ':'
+            print '\nremaining points' #, points
+            print [keys[k] for k in points]
+        
+        
+        if last_removed is not None:
+            #remove reference to last removed point in list of distances
+            for i in points:
+                indx_last_remove = -1
+                d = distances[i]
+                for j in range(len(d)):
+                    if d[j][1] == last_removed:
+                        indx_last_remove = j
+                        break
+                del(distances[i][indx_last_remove])
+        
+        if verbose:
+            print '\nk-distances'
+            for i in points:
+                dd = distances[i][1:knumber+1]
+                print keys[i],
+                print ', '.join([ "(%-5.3g to %s)"% (t1, keys[t2]) for (t1,t2) in dd])
+            print
+        
+        ksums = [sum([d[0] for d in distances[i][1:knumber+1]]) for i in points]
+        
+        #find and remove most crowded
+        distancesAndKeys = []
+        
+        allextremes = True
+        for i in points:
+            if i not in extremes:
+                allextremes = False
+                break
+
+        if not allextremes:
+            for i,k in enumerate(points):
+                if k in extremes:
+                    distancesAndKeys.append((10**300, k))
+                else:
+                    distancesAndKeys.append((ksums[i], k))
+        else:
+            for i,k in enumerate(points):
+                distancesAndKeys.append((ksums[i], k))      
+        
+        last_removed = min(distancesAndKeys)[1]
+        points.remove(last_removed)
+        mc_key = keys[last_removed]
+        
+        if verbose:
+            mcv = x[mc_key]
+            print 'Point to remove:', mc_key, mcv
+        
+        del x[mc_key]
+    
+    return x
 
 #________________________________________________________________
 #Tests for the non-dominated sorted algorithm methods
