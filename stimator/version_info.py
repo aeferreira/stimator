832f54afb91072e9bc9f2f97a17525a1b983f5de
aeferreira <ferreiae@gmail.com>
2014-02-10 10:25:16 +0000

    Advanced version in version_info.py.

--- stimator/version_info.py
+++ stimator/version_info.py
@@ -4,6 +4,6 @@ class VersionObj(object):
 
 __version__ = VersionObj()
 
-__version__.version = '0.9.885'
+__version__.version = '0.9.90'
 __version__.fullversion = __version__.version
-__version__.date = "Jan 2014"
+__version__.date = "Fev 2014"
