1100b84fc6cf3c77eb23084f0dfb1f9cdc02d04e
aeferreira <ferreiae@gmail.com>
2014-03-13 10:27:56 +0000

    Clean up of control results. moosorting.py code plugged in GDE3solver.py. Code a bit inconsistent though. Control outputs added.

--- stimator/GDE3solver.py
+++ stimator/GDE3solver.py
@@ -6,6 +6,7 @@ import numpy, timecourse
 from de import DESolver
 import dynamics
 import utils
+import moosorting
 
 def dominance(vec1, vec2):
     """Compute Pareto dominance relationship."""
@@ -196,9 +197,7 @@ class GDE3Solver(DESolver):
         self.new_generation_energies = [[] for i in range(self.populationSize)]
         self.new_population = numpy.empty((self.populationSize,len(self.toOpt)))
         self.gen_times = []
-        
-        self.dominance_rdepth = 0
-        
+
     def EnergyFunction(self, trial):
         #compute solution for each model, using trial vector
         sols = [s.solve(trial) for s in self.objFuncList]
@@ -252,11 +251,14 @@ class GDE3Solver(DESolver):
                     self.population_energies.append(difs)
                 else:
                     self.population_energies.append(energies)
+            print self.firstpop_string(20, 'after regeneration 0 before dump')
+
             timeElapsed = time() - time0
             print 'generation took', timeElapsed, 's'
             self.gen_times.append(timeElapsed)
             if self.dump_generations is not None:
                 print >> self.dumpfile, self.generation_string('0')
+            print self.firstpop_string(20, 'after regeneration 0 after dump')
         
         else: # generation >= 1
             time0 = time()
@@ -305,30 +307,33 @@ class GDE3Solver(DESolver):
             #print energyComparison
 
             #working structures for sorting
-            self.objectives = [[]]
+            self.objectives = {}
             working_sols = [numpy.array([0.0])]
             
-            n_keys = 0
+            n_keys = 1
             newBetterSols = 0
             for i in range(len(energyComparison)):
                 if energyComparison[i] > 0:
-                    self.objectives.append(self.new_generation_energies[i])
+                    self.objectives[n_keys] = self.new_generation_energies[i]
                     working_sols.append(numpy.copy(self.new_population[i]))
                     n_keys += 1
                     newBetterSols += 1
                 elif energyComparison[i] < 0:
-                    self.objectives.append(self.population_energies[i])
+                    self.objectives[n_keys] = self.population_energies[i]
                     working_sols.append(numpy.copy(self.population[i]))
                     n_keys += 1
                 else: #energyComparison[i] == 0
-                    self.objectives.append(self.population_energies[i])
-                    working_sols.append(numpy.copy(self.population[i]))
-                    self.objectives.append(self.new_generation_energies[i])
+                    self.objectives[n_keys] = self.new_generation_energies[i]
                     working_sols.append(numpy.copy(self.new_population[i]))
-                    n_keys += 2
-            self.dom_dict = {}
-            for indx in range(1,n_keys+1): # (1 based indexation)
-                self.dom_dict[indx] = []
+                    n_keys += 1
+                    self.objectives[n_keys] = self.population_energies[i]
+                    working_sols.append(numpy.copy(self.population[i]))
+                    n_keys += 1
+            
+            print '%%%%%%%%%%%%%%%%% ini OBJS'
+            for k in self.objectives:
+                print k, ' ---->', self.objectives[k]
+            print '%%%%%%%%%%%%%%%%% end OBJS'
             
             print 'New dominant solutions: %d' %(newBetterSols)
             print
@@ -336,8 +341,10 @@ class GDE3Solver(DESolver):
             print "Sorting solutions..."
 
             # sort solutions by dominance
-            self.getDominanceTree(self.dom_dict.keys())
-            nondominated_waves = self.ndf2list()
+            sorter = moosorting.MOOSorter(self.objectives)
+            
+            nondominated_waves = sorter.get_non_dominated_fronts()
+
             flengths = [len(i) for i in nondominated_waves]
             print 'Waves lengths:', flengths
             
@@ -373,6 +380,8 @@ class GDE3Solver(DESolver):
             for i in tempObjDic.keys():
                 self.population.append(working_sols[i])
                 self.population_energies.append(self.objectives[i])
+            
+            print self.firstpop_string(20, 'after regeneration')
             
             timeElapsed = time() - sortingtime
             print 'done, took %6.3f'% timeElapsed, 's'
@@ -416,6 +425,14 @@ class GDE3Solver(DESolver):
     "Solution found by convergence criterium",
     "Too many generations with only non-dominant solutions")
 
+    def firstpop_string(self, nelems, msg = ""):
+        res = 'CONTROL %s -------------------------\n'%msg
+        for s,o in zip(self.population[:nelems], self.population_energies[:nelems]):
+            sstr = ' '.join([str(i) for i in s])
+            ostr = ' '.join([str(i) for i in o])
+            res = res + '%s %s\n'%(sstr, ostr)
+        return res
+
     def generation_string(self, generation):
         generation = str(generation)
         res = 'generation %s -------------------------\n'%generation
@@ -440,166 +457,6 @@ class GDE3Solver(DESolver):
             if self.generation-1 not in self.dump_generations:
                 print >> self.dumpfile, self.generation_string(self.generation-1)
             self.dumpfile.close()
-
-        
-        #self.reportFinal()
-
-    #------------------------------------------------------------------------------------------------------------------------------------
-    #This code is an adaptation of the non-dominated sorting algorithm with delayed insertion published in
-    #Fang et al (2008) An Efficient Non-dominated Sorting Method for Evolutionary Algorithms, Evolutionary Computation 16(3):355-384
-
-    def depthspaces(self):
-        return ' '*(self.dominance_rdepth-1)*4
-    
-    def getDominanceTree(self, nodeList, verbose = False):
-        """ This function applies the 'Divide and Conquer' method recursively generating
-        the dominance tree (divides) and returning the product of the mergeDominanceTree function (conquers).
-        This version doesn't use delayed insertion of dominated node yet."""
-        self.dominance_rdepth += 1
-        if verbose:
-            spcs = self.depthspaces()
-            print spcs, '---------------------------------'
-            print spcs, 'IN getDominanceTree()'
-            print spcs, 'nodes', nodeList
-        size = len(nodeList)
-        if size > 1:
-            leftTree  = self.getDominanceTree(nodeList[:size/2], verbose)
-            rightTree = self.getDominanceTree(nodeList[ size/2:], verbose)
-            if verbose:
-                print spcs, '@@  left tree', leftTree
-                print spcs, '@@ right tree', rightTree
-                print spcs, '@@ dom dict', self.dom_dict
-            res = self.merge_Dom_trees(leftTree, rightTree)
-            if verbose:
-                print spcs, '--> return from merge', res
-                print spcs, '--> dom dict', self.dom_dict
-            self.dominance_rdepth -= 1
-            return res
-        else:
-            if verbose:
-                print spcs, '.single node return', nodeList
-            self.dominance_rdepth -= 1
-            return nodeList
-
-    def merge_Dom_trees(self, left_tree, right_tree):
-        """ This function merges (conquers) the dominance trees recursively (not using delayed insertion yet). """
-        if len(left_tree) == 0:
-            if len(right_tree) < 2:
-                return right_tree
-        left = Node(left_tree[0])
-        right = Node(right_tree[0])
-        one_dominates = False 
-        while left.valid() and right.valid(): #and left_tree != [] and right_tree != []:
-            one_dominates = False
-            d = dominance(self.objectives[right.indx], self.objectives[left.indx])
-            if d < 0: 
-                one_dominates = True
-                node = right.indx
-                right.move_and_remove(right_tree) 
-                for k in self.dom_dict:
-                    if node in self.dom_dict[k]:
-                        self.dom_dict[k].remove(node)
-                self.dom_dict[left.indx] = self.merge_Dom_trees(self.dom_dict[left.indx], [node])
-                if left.valid() and not right.valid() and right_tree != []: #New!
-                    right.indx = right_tree[0]
-            elif d > 0:
-                one_dominates = True
-                node = left.indx
-                left.move_and_remove(left_tree) 
-                for k in self.dom_dict:
-                    if node in self.dom_dict[k]:
-                        self.dom_dict[k].remove(node)
-                self.dom_dict[right.indx] = self.merge_Dom_trees(self.dom_dict[right.indx], [node])
-                if left_tree == []:
-                    left_tree = right_tree
-                    right_tree = []
-                else:
-                    right.indx = right_tree[0] #This is necessary for the new element from left_tree to be compared with every element os right_tree
-            else:
-                right.point_to_next_sibling(right_tree)
-                if not right.valid():
-                    right.indx = right_tree[0]
-                    left.point_to_next_sibling(left_tree)
-        if not one_dominates:
-            left.indx = left_tree[0]
-            right.indx = right_tree[0]
-        while right_tree != [] and right.valid():
-            left_tree.append(right.indx)
-            right.move_and_remove(right_tree)
-            if not right.valid() and len(right_tree) > 0:
-                left_tree.extend(right_tree)
-        #This block merges cousins, i.e. the nodes at the same non-dominance level which are children of different non-dominant siblings
-        #I'm not sure if this should be done every time or just in the final.
-        if self.dom_dict != {}:
-            firstFrontToBeCompared = [0, 0]
-            grandChildren = []
-            #This finds and merges cousins which are children of dominated solutions
-            for i in left_tree:
-                if self.dom_dict[i] != []:
-                    grandChildren.append(self.dom_dict[i])
-                    if len(grandChildren) == 2:
-                        grandChildren = [self.merge_Dom_trees(grandChildren[0], grandChildren[1])]
-            #Continues to merge the cousins which are children of non-dominated parents
-            while len(firstFrontToBeCompared) > 1:
-                firstFrontToBeCompared = []
-                for i in left_tree:
-                    nonDominantSolution = True
-                    for k in self.dom_dict:
-                        if i in self.dom_dict[k]:
-                            nonDominantSolution = False
-                    if nonDominantSolution and self.dom_dict[i]!=[]:
-                        firstFrontToBeCompared.append(i)
-                    if len(firstFrontToBeCompared) > 1:
-                        #Next two lines are necessary to avoid errors inside merge function
-                        cousin1 = self.dom_dict[firstFrontToBeCompared[0]]
-                        cousin2 = self.dom_dict[firstFrontToBeCompared[1]]
-                        self.merge_Dom_trees(cousin1, cousin2)
-                        break
-        return left_tree
-
-    def ndf2list(self):
-        """Organizes a list of nondominated fronts from self.dom_dict"""
-        nonDominatedFronts = []
-        while len(self.dom_dict) > 0:
-            allvls = []
-            for v in self.dom_dict.values():
-                allvls.extend(v)
-            nonDominated = [k for k in self.dom_dict if k not in allvls]
-            nonDominatedFronts.append(nonDominated)
-            for k in nonDominated:
-                del self.dom_dict[k]
-        return nonDominatedFronts
-
-    #------------------------------------------------------------------------------------------------------------------------------------
-    #End of non-dominated sorted algorithm methods
-
-
-class Node:
-    """Node object, which basically contains an index that can be incremented 
-    by method point_to_next_sibling to iterate over a list of sibling nodes in a tree.
-    """
-
-    def __init__(self, nodeIndex):
-        """Initializes Node object with an index"""
-        self.indx = nodeIndex
-
-    def point_to_next_sibling(self, tree):
-        """Increments the index to the next sibling of the node, if not past the end of the 'tree' list"""
-        indx = tree.index(self.indx)
-        if indx < len(tree)-1:
-            self.indx = tree[indx+1]
-        else:
-            self.indx = -1
-    
-    def valid(self):
-        return self.indx != -1
-        
-    def move_and_remove(self, tree):
-        node = self.indx
-        self.point_to_next_sibling(tree)
-        tree.remove(node)
-        
-        
 
 def removeMostCrowded(x, knumber = 3, remove_n = 1, verbose = False):
 
@@ -735,123 +592,3 @@ def removeMostCrowded(x, knumber = 3, remove_n = 1, verbose = False):
         del x[mc_key]
     
     return x
-
-#________________________________________________________________
-#Tests for the non-dominated sorted algorithm methods
-
-from scipy.spatial.distance import squareform, pdist
-if __name__ == "__main__":
-
-    class FangEtAL_test(GDE3Solver):
-        def __init__(self, report = False):
-            data = [[182.08, 100.13, 192.21],[187.53, 246.16, 203.2],
-                    [197.15, 201.57, 318.86],[47.48, 74.96, 22.69],
-                    [37.05, 304.83, 381.19], [126.88, 54.58, 144.17],
-                    [101.77, 49.18, 111.91], [37.47, 18.63, 446.57]]
-            
-            print 'distance matrix'
-            print squareform(pdist(data, dominance))
-            print '##########################################'
-            print
-
-            self.dominance_rdepth = 0
-            n_nodes = len(data)
-            self.n_objectives = len(data[0])
-            print 'Test initialized'
-            print 'Nodes: %d  Objectives: %d' % (n_nodes, self.n_objectives)
-            #dict keys are integers that begin at 1.
-            self.dom_dict = {}
-            self.objectives = {}
-            for i_node in range(n_nodes):
-                self.dom_dict[i_node+1] = []
-                self.objectives[i_node+1] = data[i_node]
-            keys = self.objectives.keys()
-
-            print 'objectives dict: %d keys with %d elements' % (len(self.objectives), len(self.objectives[1]))
-            print 'self.dom_dict and objectiveDic created.'
-            print '\nComputing nondominated_waves...'
-            self.getDominanceTree(keys, verbose = True)
-            print 'DOMINANCE DICT'
-            for k in self.dom_dict:
-                print k, '>', self.dom_dict[k]
-            nondominated_waves = self.ndf2list()
-            print '%d nondominated_waves:'% len(nondominated_waves)
-            for wave in nondominated_waves:
-                print wave
-            print '======================================================'
-            print 'Comparing to published results...'
-            published =[[4, 5, 7, 8],[6],[1],[2, 3]]
-            if nondominated_waves == published:
-                print 'PASSED'
-            else:
-                print 'FAILED'
-            print '======================================================'
-
-    class ndsaTest(GDE3Solver):
-
-        def __init__(self, n_nodes, n_objectives, report = False):
-            self.dominance_rdepth = 0
-            self.n_nodes = n_nodes
-            self.n_objectives = n_objectives
-            print 'Test initialized'
-            print 'Nodes: %d  Objectives: %d' % (n_nodes, n_objectives)
-            numpy.random.seed(2)
-            #dict keys are integers that begin at 1.
-            self.dom_dict = {}
-            self.objectives = {}
-            for i_node in range(self.n_nodes):
-                self.dom_dict[i_node+1] = []
-                self.objectives[i_node+1] = []
-                for i_objective in range(self.n_objectives):
-                    self.objectives[i_node+1].append(numpy.random.rand())
-            keys = self.objectives.keys()
-
-            print 'objectives dict: %d keys with %d elements' % (len(self.objectives), len(self.objectives[1]))
-            print 'self.dom_dict and objectiveDic created.'
-            print '\nComputing nondominated_waves...'
-            self.getDominanceTree(keys, verbose = True)
-            print 'DOMINANCE DICT'
-            for k in self.dom_dict:
-                print k, '>', self.dom_dict[k]
-            nondominated_waves = self.ndf2list()
-            print '%d nondominated_waves:'% len(nondominated_waves)
-            for wave in nondominated_waves:
-                print wave
-            print '\nTesting non-dominance between solutions in the same front...',
-            for wave in nondominated_waves:
-                if len(wave) == 0:
-                    print '\n FAILED: empty front found!'
-                    return
-                if len(wave) == 1:
-                    pass
-                else:
-                    for p in range(len(wave)-1):
-                        for r in range(p+1, len(wave)):
-                            d = dominance(self.objectives[wave[r]], self.objectives[wave[p]])
-                            if d != 0:
-                                print '\n FAILED:'
-                                print 'Domination relationship in front', wave, 'between nodes', wave[p], 'and', wave[r],'. Test not passed.\n\n'
-                                return
-            print 'passed.'
-            if len(nondominated_waves) == 1:
-                print 'Only non-dominated solutions - no test between different fronts'
-                return
-            else:
-                print 'Testing dominance relationship between solutions in different fronts...',
-                #Solution in rFront must be dominated by at least one solution in pFront and cannot dominate any solution in pFront.
-                for pFront in range(len(nondominated_waves)-1):
-                    for rFront in range(pFront+1, len(nondominated_waves)):
-                        for down in nondominated_waves[rFront]:
-                            for up in nondominated_waves[pFront]:
-                                d = dominance(self.objectives[down], self.objectives[up])
-                                if d == 1:
-                                    print '\n FAILED:'
-                                    print 'Solution in front', pFront, ', (', up, ') is dominated by solution in front', rFront, ', (', down, '). Test not passed.\n\n'
-                                    return
-                print 'passed.'
-    
-##     i = 50
-##     j = 2
-##     ndsaTest(i, j)
-    FangEtAL_test()
-    
