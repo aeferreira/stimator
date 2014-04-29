bfa73891220f08e32716f6b0da932ea4d5cee1b1
aeferreira <ferreiae@gmail.com>
2014-04-29 08:41:16 +0100

    root batch files added.

---
+++ gen_portable.bat
@@ -0,0 +1,5 @@
+xcopy run_wxgui.py portable
+xcopy start_stimator.bat portable
+xcopy stimator\*.py portable\stimator /s/i
+xcopy stimator\examples\*.mdl portable\stimator\examples /i
+xcopy stimator\examples\*.txt portable\stimator\examples /i
---
+++ start_stimator.bat
@@ -0,0 +1,3 @@
+:: set path=c:\Miniconda\Scripts;%path%
+:: set path=c:\Miniconda;%path%
+python run_wxgui.py
