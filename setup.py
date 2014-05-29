fc7bec7049e37e69748b9782bf55fdad1b18fba9
aeferreira <ferreiae@gmail.com>
2014-05-29 13:57:27 +0100

    Classifiers added to setup.py.

--- setup.py
+++ setup.py
@@ -19,18 +19,39 @@ packages = [
 
 requires = ['sympy']
 
+classifs=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Scientific/Engineering :: Artificial Life',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Scientific/Engineering :: Physics']
     
 setup(name = "stimator",
     version = "0.9.85",
     license = "BSD",
-    description = "Analysis of ODE models with focus on model selection and parametrization.",
+    description = "Analysis of ODE models with focus on model selection and parameter estimation.",
     author = "António Ferreira",
     author_email = "aeferreira@fc.ul.pt",
     url = "http://enzymology.fc.ul.pt/software.htm",
     include_package_data=True,
     packages = packages,
-    package_data={'stimator': ['examples/*.*']},
+    package_data={'stimator': ['examples/*.py', 
+                               'examples/*.ipynb', 
+                               'examples/*.mdl', 
+                               'examples/*.txt']},
     scripts = ["run_wxgui.py"],
+    keywords = "hello world example examples",
+    classifiers=classifs,
     long_description = readme,
     install_requires = requires)
 
