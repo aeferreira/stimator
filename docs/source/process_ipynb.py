f8ff6fa1ad184e664ed5b1bd5d38265dad4574b9
aeferreira <ferreiae@gmail.com>
2014-07-15 21:16:27 +0100

    Improvement in the process_ipynb.py.

--- docs/source/process_ipynb.py
+++ docs/source/process_ipynb.py
@@ -3,44 +3,46 @@ import subprocess
 import shutil
 import os
 
-nbfiles = 'solving.ipynb'
+nbfiles = ['solving.ipynb']
 
 def process_list(nbfiles):
-    exe_nbfiles = nbfiles.replace('.ipynb', '_executed.ipynb')
-    print ('--------- executing notebook {0} ------------'.format(nbfiles))
-    aaa = ['runipy', nbfiles, exe_nbfiles]
-    print (subprocess.check_output(aaa))
-
-    for n in [nbfiles, exe_nbfiles]:
-        print ('--------- converting notebook {0} -----------'.format(n))
-        aaa = ('ipython nbconvert --to rst --template non_exec_rst.tpl %s'%n).split()
+    for nbf in nbfiles:
+        fname, ext = nbf.split('.')
+        exe_nbf = nbf.replace('.ipynb', '_executed.ipynb')
+        print ('--------- executing notebook {0} ------------'.format(nbf))
+        aaa = ['runipy', nbf, exe_nbf]
         print (subprocess.check_output(aaa))
 
-    rst_exe_nbfiles = exe_nbfiles.replace('.ipynb', '.rst')
-    print ('--------- fixing img links in {0} ---------'.format(rst_exe_nbfiles))
+        for n in [nbf, exe_nbf]:
+            print ('--------- converting notebook {0} -----------'.format(n))
+            aaa = ('ipython nbconvert --to rst --template non_exec_rst.tpl %s'%n).split()
+            print (subprocess.check_output(aaa))
 
-    print ('reading %s'% rst_exe_nbfiles)
-    with open (rst_exe_nbfiles) as f:
-        alltext = f.read()
+        rst_exe_nbf = exe_nbf.replace('.ipynb', '.rst')
+        print ('--------- fixing img links in {0} ---------'.format(rst_exe_nbf))
 
-    alltext = alltext.replace('.. image:: solving_executed_files%5C','.. image:: _static/solving/')
+        print ('reading %s'% rst_exe_nbf)
+        with open (rst_exe_nbf) as f:
+            alltext = f.read()
 
-    print ('writing %s'% rst_exe_nbfiles)
-    with open (rst_exe_nbfiles, 'w') as f:
-        f.write(alltext)
+        alltext = alltext.replace('.. image:: %s'% fname + '_executed_files%5C','.. image:: _static/%s/' % fname)
 
-    print ('------------- copying files to static dir -----------')
-    ddest = '_static\solving'
-    try:
-        os.makedirs(ddest)
-    except Exception:
-        pass
-    files = os.listdir( './solving_executed_files' )
-    for f in files:
-        f = os.path.join('./solving_executed_files',f)
-        print 'copying', f, 'to', ddest
-        shutil.copy(f, ddest)
-    print ('--------- Done -----------------------------')
+        print ('writing %s' % rst_exe_nbf)
+        with open (rst_exe_nbf, 'w') as f:
+            f.write(alltext)
+
+        print ('------------- copying files to static dir -----------')
+        ddest = '_static\%s' % fname
+        try:
+            os.makedirs(ddest)
+        except Exception:
+            pass
+        files = os.listdir( './%s_executed_files' % fname )
+        for f in files:
+            f = os.path.join('./%s_executed_files' % fname,f)
+            print 'copying', f, 'to', ddest
+            shutil.copy(f, ddest)
+        print ('--------- Done -----------------------------')
 
 if __name__ == '__main__':
     process_list(nbfiles)
