7a9f899a3e66115619feda730744bb52ba8068c8
aeferreira <ferreiae@gmail.com>
2018-10-04 16:24:58 +0100

    Bump version.

--- stimator/__init__.py
+++ stimator/__init__.py
@@ -9,7 +9,7 @@ from stimator.model import Model
 from stimator.modelparser import read_model
 import stimator.examples as examples
 
-__version__ = '0.9.110'
+__version__ = '0.9.120'
 
 sys.path.insert(0, os.path.split(os.path.split(os.path.abspath(__file__))[0])[0])
 
@@ -19,7 +19,7 @@ class VersionObj(object):
     def __init__(self):
         self.version = __version__
         self.fullversion = self.version
-        self.date = "Mar 2018"
+        self.date = "Oct 2018"
 
     def __str__(self):
         return self.version
