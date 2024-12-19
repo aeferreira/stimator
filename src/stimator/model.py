733bb0699c275086d8096498e0760898e101f31a
aeferreira <ferreiae@gmail.com>
2024-12-19 18:33:58 +0000

    Simplification in exception handling in parsing of rate expressions.

--- src/stimator/model.py
+++ src/stimator/model.py
@@ -1270,11 +1270,12 @@ def _check_rates(model):
     for v in chain(
         model.input_variables, model.reactions, model.transformations
     ):
-        msg, value = _parse_rate(model, v)
-        if msg != "":
-            return False, f"{msg:s}\nin rate of {v.name}: {v()}"
-        else:
+        try:
+            value = _parse_rate(model, v)
             v._value = value
+        except Exception as e:
+            expt_txt = f"{str(e.__class__.__name__)} : {str(e)}"
+            return False, f"{expt_txt:s}\nin rate of {v.name}: {v()}"
     return True, "OK"
 
 
@@ -1303,21 +1304,22 @@ def _parse_rate(model, rate_obj):
     expr = rate_obj()
     locs = dict(_generate_local_dict(model, rate_obj))
 
-    try:
-        vardict = {name: sympy.Symbol(name) for name in model.varnames}
-        vardict["t"] = sympy.Symbol('t')
-        locs.update(vardict)
+    # try:
+    vardict = {name: sympy.Symbol(name) for name in model.varnames}
+    vardict["t"] = sympy.Symbol('t')
+    locs.update(vardict)
 
-        value = parse_expr(
-        expr,
-        locs,
-        global_dict=allowed_sympy_funcs,
-        transformations=T[4],
-        )
-        # value = float(eval(expr, model._usable_functions, locs))
-    except Exception as e:
-        return ("%s : %s" % (str(e.__class__.__name__), str(e)), 0.0)
-    return "", value
+    value = parse_expr(
+    expr,
+    locs,
+    global_dict=allowed_sympy_funcs,
+    transformations=T[4],
+    )
+    return value
+    # value = float(eval(expr, model._usable_functions, locs))
+    # except Exception as e:
+    #     return ("%s : %s" % (str(e.__class__.__name__), str(e)), 0.0)
+    # return "", value
 
 
 def parse_const(model, parsed_name, valueexpr):
